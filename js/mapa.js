169c5638d9cddb6709a74877acb0c3ae3ba2eb6c
Alternhuman <diegomartinpi@gmail.com>
2014-08-15 23:25:17 +0200

    Canarias añadidas Corregidos errores en unis.json

--- js/mapa.js
+++ js/mapa.js
@@ -52,6 +52,43 @@ $(document).ready(function() {
             .attr("class", "subunit-boundary");
     });
 
+    var projection_canary = d3.geo.albers()
+        .center([-3, 21.5])
+        .rotate([3.4, 0])
+        .parallels([50, 51])
+        .scale(1200 * 1.3)
+        .translate([width / 2.5, height / 2]);
+
+    var path_canary = d3.geo.path().projection(projection_canary);
+
+
+    var canarias = d3.select("#map-canarias")
+            .append("svg")
+            .attr("width", 150)
+            .attr("height", 100);
+            canarias.append("g")
+            .attr("width", 100)
+            .attr("height", 100)
+
+    d3.json("maps/canary.json", function(error, esp) {
+        canarias.selectAll(".subunit")
+            .data(topojson.feature(esp, esp.objects.subunits).features)
+            .enter().append("path")
+            .attr("class", function(d) {
+                return "subunit " + d.id;
+            })
+            .attr("d", path_canary)
+            .on("mouseover", provincia_hover)
+            .on("click", provincia_click);
+
+        canarias.append("path")
+            .datum(topojson.mesh(esp, esp.objects.subunits, function(a, b) {
+                return a !== b
+            }))
+            .attr("d", path)
+            .attr("class", "subunit-boundary");
+    });
+
     d3.select(window).on('resize', sizeChange);
     sizeChange();
 
@@ -65,9 +102,9 @@ $(document).ready(function() {
             $("#bootstrap_lista_units").attr("class", "well col-xs-5 col-sm-5 col-md-5 col-lg-5");
         }
 
-        d3.select("g").attr("transform", "scale(" + $("#map").width() / 700 + ")");
-        $("svg").height($("#map").width() * 0.618);
-        $("svg").width($("#map").width());
+        d3.select("#map>svg>g").attr("transform", "scale(" + $("#map").width() / 700 + ")");
+        $("#map>svg").height($("#map").width() * 0.618);
+        $("#map>svg").width($("#map").width());
     }
 
     function provincia_hover(d) {
@@ -161,39 +198,41 @@ $(document).ready(function() {
                     centro: value.centro,
                     url: value.url,
                     siglas: value.siglas.replace(/-.*/, ''),
-                    tasas1: value.tasas1,
-                    tasas2: value.tasas2,
-                    tasas3: value.tasas3,
-                    tasas4: value.tasas4
+                    tasas1: value.tasas_2014.tasas1,
+                    tasas2: value.tasas_2014.tasas2,
+                    tasas3: value.tasas_2014.tasas3,
+                    tasas4: value.tasas_2014.tasas4
                 });
                 //console.log(value.tasas4);
 
                 $('#bootstrap_lista_units').append(render_resultados);
                 console.log(value);
-                var chart = c3.generate({
-                    bindto: "#chart",
-                    data: {
-                        x: 'x',
-                        x_format: '%Y',
-                        columns: [
-                            ['x', new Date('2011'), new Date('2012'), new Date('2013'), new Date('2014')],
-                            ['Primera matrícula', value.tasas_2011.tasas1, value.tasas_2012.tasas1, value.tasas_2013.tasas1, value.tasas_2014.tasas1],
-                            ['Segunda matrícula', value.tasas_2011.tasas2, value.tasas_2012.tasas2, value.tasas_2013.tasas2, value.tasas_2014.tasas2],
-                            ['Tercera matrícula', value.tasas_2011.tasas3, value.tasas_2012.tasas3, value.tasas_2013.tasas3, value.tasas_2014.tasas3],
-                            ['Cuarta matrícula', value.tasas_2011.tasas4, value.tasas_2012.tasas4, value.tasas_2013.tasas4, value.tasas_2014.tasas4],
-                            ['Media nacional', 0, 0, 0, 0, 0, 0]
-                        ]
-                    },
-                    axis: {
-                        x: {
-                            type: 'timeseries',
-                            tick: {
-                                //              format : "%m/%d" // https://github.com/mbostock/d3/wiki/Time-Formatting#wiki-format
-                                format: "%Y" // https://github.com/mbostock/d3/wiki/Time-Formatting#wiki-format
+                if (value.tasas_2011 && value.tasas_2012 && value.tasas_2013) {
+                    var chart = c3.generate({
+                        bindto: "#chart",
+                        data: {
+                            x: 'x',
+                            x_format: '%Y',
+                            columns: [
+                                ['x', new Date('2011'), new Date('2012'), new Date('2013'), new Date('2014')],
+                                ['Primera matrícula', value.tasas_2011.tasas1, value.tasas_2012.tasas1, value.tasas_2013.tasas1, value.tasas_2014.tasas1],
+                                ['Segunda matrícula', value.tasas_2011.tasas2, value.tasas_2012.tasas2, value.tasas_2013.tasas2, value.tasas_2014.tasas2],
+                                ['Tercera matrícula', value.tasas_2011.tasas3, value.tasas_2012.tasas3, value.tasas_2013.tasas3, value.tasas_2014.tasas3],
+                                ['Cuarta matrícula', value.tasas_2011.tasas4, value.tasas_2012.tasas4, value.tasas_2013.tasas4, value.tasas_2014.tasas4],
+                                ['Media nacional', 0, 0, 0, 0, 0, 0]
+                            ]
+                        },
+                        axis: {
+                            x: {
+                                type: 'timeseries',
+                                tick: {
+                                    //              format : "%m/%d" // https://github.com/mbostock/d3/wiki/Time-Formatting#wiki-format
+                                    format: "%Y" // https://github.com/mbostock/d3/wiki/Time-Formatting#wiki-format
+                                }
                             }
                         }
-                    }
-                });
+                    });
+                }
 
                 var panels = $('.user-infos');
                 var panelsButton = $('.dropdown-user');
