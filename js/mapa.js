9aae8aa80d789783fcfa466ef37181591467da6f
Alternhuman <diegomartinpi@gmail.com>
2014-08-16 09:57:33 +0200

    Minor changes

--- js/mapa.js
+++ js/mapa.js
@@ -1,117 +1,117 @@
 $(document).ready(function() {
-    //Diseño responsive
-    if ($(window).width() < 480) {
-        $("#content").attr('class', "row");
-        $("#map").attr("class", "well well-lg");
-        $("#bootstrap_lista_units").attr("class", "well");
-    } else {
-        $("#content").attr('class', "");
-        $("#map").attr("class", "well well-lg col-xs-5 col-sm-5 col-md-5 col-lg-5 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1");
-        $("#bootstrap_lista_units").attr("class", "well col-xs-5 col-sm-5 col-md-5 col-lg-5");
-    }
-
-    /*Creación del mapa*/
-
-    var width = 760;
-    var height = 470;
-
-    //Proyección Albers de los datos con ajustes para la Península
-    var projection = d3.geo.albers()
-        .center([0, 39.23])
-        .rotate([3.4, 0])
-        .parallels([50, 90])
-        .scale(1200 * 2.3)
-        .translate([width / 2.5, height / 2]);
-
-    /*Creacion de un path según los ajustes de proyección*/
-    var path = d3.geo.path()
-        .projection(projection);
-
-    /*Se añade un svg al div que contendrá el mapa*/
-    var svg = d3.select("#map")
-        .append("svg")
-        .append("g")
-        .attr("width", width)
-        .attr("height", height);
-
-    /*Se añaden una a una las provincias descritas en el json*/
-    d3.json("maps/esp-ascii.json", function(error, esp) {
-        svg.selectAll(".subunit")
-            .data(topojson.feature(esp, esp.objects.subunits).features)
-            .enter().append("path")
-            .attr("class", function(d) {
-                return "subunit " + d.id;
-            })
-            .attr("d", path)
-            .on("mouseover", provincia_hover)
-            .on("click", provincia_click);
-
-        svg.append("path")
-            .datum(topojson.mesh(esp, esp.objects.subunits, function(a, b) {
-                return a !== b
-            }))
-            .attr("d", path)
-            .attr("class", "subunit-boundary");
-    });
-
-    var projection_canary = d3.geo.albers()
-        .center([-6.5, 24])
-        .rotate([3.4, 0])
-        .parallels([50, 51])
-        .scale(1200 * 1.9)
-        .translate([width / 2.5, height / 2]);
-
-    var path_canary = d3.geo.path().projection(projection_canary);
-
-
-    width_canary = $("#map-canarias").width();
-    height_canary = $("#map-canarias").height();
-    var canarias = d3.select("#map-canarias")
-        .append("svg")
-        .append("g")
-        .attr("width", width_canary)
-        .attr("height", height_canary);
-
-    d3.json("maps/canary.json", function(error, esp) {
-        canarias.selectAll(".subunit")
-            .data(topojson.feature(esp, esp.objects.subunits).features)
-            .enter().append("path")
-            .attr("class", function(d) {
-                return "subunit " + d.id;
-            })
-            .attr("d", path_canary)
-            .on("mouseover", provincia_hover)
-            .on("click", provincia_click);
-
-        canarias.append("path")
-            .datum(topojson.mesh(esp, esp.objects.subunits, function(a, b) {
-                return a !== b
-            }))
-            .attr("d", path)
-            .attr("class", "subunit-boundary");
-    });
-
-    d3.select(window).on('resize', sizeChange);
-    sizeChange();
-
-    function sizeChange() {
+        //Diseño responsive
         if ($(window).width() < 480) {
             $("#content").attr('class', "row");
             $("#map").attr("class", "well well-lg");
             $("#bootstrap_lista_units").attr("class", "well");
         } else {
+            $("#content").attr('class', "");
             $("#map").attr("class", "well well-lg col-xs-5 col-sm-5 col-md-5 col-lg-5 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1");
             $("#bootstrap_lista_units").attr("class", "well col-xs-5 col-sm-5 col-md-5 col-lg-5");
         }
 
-        d3.select("#map>svg>g").attr("transform", "scale(" + $("#map").width() / 700 + ")");
-        $("#map>svg").height($("#map").width() * 0.618);
-        $("#map>svg").width($("#map").width());
+        /*Creación del mapa*/
 
-        d3.select("#map-canarias>svg>g").attr("transform", "scale(" + $("#map-canarias").width() / 200 + ")");
-        $("#map-canarias>svg").height($("#map-canarias").width() * 0.518);
-        $("#map-canarias>svg").width($("#map-canarias").width());
-    }
+        var width = 760;
+        var height = 470;
+
+        //Proyección Albers de los datos con ajustes para la Península
+        var projection = d3.geo.albers()
+            .center([0, 39.23])
+            .rotate([3.4, 0])
+            .parallels([50, 90])
+            .scale(1200 * 2.3)
+            .translate([width / 2.5, height / 2]);
+
+        /*Creacion de un path según los ajustes de proyección*/
+        var path = d3.geo.path()
+            .projection(projection);
+
+        /*Se añade un svg al div que contendrá el mapa*/
+        var svg = d3.select("#map")
+            .append("svg")
+            .append("g")
+            .attr("width", width)
+            .attr("height", height);
+
+        /*Se añaden una a una las provincias descritas en el json*/
+        d3.json("maps/esp-ascii.json", function(error, esp) {
+            svg.selectAll(".subunit")
+                .data(topojson.feature(esp, esp.objects.subunits).features)
+                .enter().append("path")
+                .attr("class", function(d) {
+                    return "subunit " + d.id;
+                })
+                .attr("d", path)
+                .on("mouseover", provincia_hover)
+                .on("click", provincia_click);
+
+            svg.append("path")
+                .datum(topojson.mesh(esp, esp.objects.subunits, function(a, b) {
+                    return a !== b
+                }))
+                .attr("d", path)
+                .attr("class", "subunit-boundary");
+        });
+
+        var projection_canary = d3.geo.albers()
+            .center([-6.5, 24])
+            .rotate([3.4, 0])
+            .parallels([50, 51])
+            .scale(1200 * 1.9)
+            .translate([width / 2.5, height / 2]);
+
+        var path_canary = d3.geo.path().projection(projection_canary);
+
+
+        width_canary = $("#map-canarias").width();
+        height_canary = $("#map-canarias").height();
+        var canarias = d3.select("#map-canarias")
+            .append("svg")
+            .append("g")
+            .attr("width", width_canary)
+            .attr("height", height_canary);
+
+        d3.json("maps/canary.json", function(error, esp) {
+            canarias.selectAll(".subunit")
+                .data(topojson.feature(esp, esp.objects.subunits).features)
+                .enter().append("path")
+                .attr("class", function(d) {
+                    return "subunit " + d.id;
+                })
+                .attr("d", path_canary)
+                .on("mouseover", provincia_hover)
+                .on("click", provincia_click);
+
+            canarias.append("path")
+                .datum(topojson.mesh(esp, esp.objects.subunits, function(a, b) {
+                    return a !== b
+                }))
+                .attr("d", path)
+                .attr("class", "subunit-boundary");
+        });
+
+        d3.select(window).on('resize', sizeChange);
+        sizeChange();
+
+        function sizeChange() {
+            if ($(window).width() < 480) {
+                $("#content").attr('class', "row");
+                $("#map").attr("class", "well well-lg");
+                $("#bootstrap_lista_units").attr("class", "well");
+            } else {
+                $("#map").attr("class", "well well-lg col-xs-5 col-sm-5 col-md-5 col-lg-5 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1");
+                $("#bootstrap_lista_units").attr("class", "well col-xs-5 col-sm-5 col-md-5 col-lg-5");
+            }
+
+            d3.select("#map>svg>g").attr("transform", "scale(" + $("#map").width() / 700 + ")");
+            $("#map>svg").height($("#map").width() * 0.618);
+            $("#map>svg").width($("#map").width());
+
+            d3.select("#map-canarias>svg>g").attr("transform", "scale(" + $("#map-canarias").width() / 200 + ")");
+            $("#map-canarias>svg").height($("#map-canarias").width() * 0.518);
+            $("#map-canarias>svg").width($("#map-canarias").width());
+        }
 
     function provincia_hover(d) {
 
@@ -124,38 +124,6 @@ $(document).ready(function() {
                 return m.id;
             }
         });
-
-        /*d3.json("data/uni/unis.json", function(error, unis) {
-
-            nombre_provincia = d.id;
-            var universidades = unis.unis;
-
-            $.each(unis.unis, function(index, value) {
-                var provincia = value.provincia;
-                var convenios;
-                if (value.provincia === d.id) {
-                    convenios = value.convenios;
-                    $.each(convenios, function(index, secondValue) {
-                        $.each(universidades, function(index, thirdValue) {
-                            if (thirdValue.siglas === secondValue) {
-                                $("." + thirdValue.provincia).attr("class", "subunit " + thirdValue.provincia + " sicue");
-                            }
-                        });
-                    });
-                }
-            });
-        });*/
-
-    }
-
-    /*function provincia_hover_out(d) {
-        var sicue = $('.sicue');
-        $.each($('.subunit'), function(index, value) {
-            clase = $(this).attr('class');
-            clase = clase.replace(/sicue/g, '');
-            $(this).attr('class', clase);
-        });
-    }*/
 
     function provincia_click(d) {
         var resultados = [];
@@ -174,7 +142,6 @@ $(document).ready(function() {
                 $.each(resultados, function(index, value) {
                     universidades_provincia.push(resultados[index]);
                     for (var i = universidades.length - 1; i >= 0; i--) {
-                        //console.log(universidades[i].siglas);
                         if (resultados[index].convenios.indexOf(universidades[i].siglas) > -1) {
                             convenios_filter.push(universidades[i]);
                         }
@@ -195,7 +162,6 @@ $(document).ready(function() {
              */
             var siglas = value.siglas.replace(/\-.*/g, '');
             var render_convenios;
-            //console.log(value);
 
             $.get('template_universidad_provincia.mst', function(template_universidad_provincia) {
                 var render_resultados = Mustache.render(template_universidad_provincia, {
@@ -208,17 +174,22 @@ $(document).ready(function() {
                     tasas2: value.tasas_2014.tasas2,
                     tasas3: value.tasas_2014.tasas3,
                     tasas4: value.tasas_2014.tasas4,
-                    urls:[
-                        {"url":value.tasas_2011.url,"fecha":2011},
-                        {"url":value.tasas_2012.url,"fecha":2012},
-                        {"url":value.tasas_2013.url,"fecha":2013},
-                        {"url":value.tasas_2014.url,"fecha":2014}
-                    ]
+                    urls: [{
+                        "url": value.tasas_2011.url,
+                        "fecha": 2011
+                    }, {
+                        "url": value.tasas_2012.url,
+                        "fecha": 2012
+                    }, {
+                        "url": value.tasas_2013.url,
+                        "fecha": 2013
+                    }, {
+                        "url": value.tasas_2014.url,
+                        "fecha": 2014
+                    }]
                 });
-                //console.log(value.tasas4);
 
                 $('#bootstrap_lista_units').append(render_resultados);
-                //console.log(value);
                 if (value.tasas_2011 && value.tasas_2012 && value.tasas_2013) {
                     var chart = c3.generate({
                         bindto: "#chart",
