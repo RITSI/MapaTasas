efa4be9d5bcd402054bf92cec6ef12f08365a107
Martin <martinarroyo@usal.es>
2016-03-01 17:07:38 +0100

    Auxiliar files

--- tasasrest/settings.py
+++ tasasrest/settings.py
@@ -148,3 +148,8 @@ ESCUDOS_PATH = 'escudos'
 MIN_YEAR = 2011 # Primer año válido para introducir tasas
 YEARS_IN_ADVANCE = 1 # Número de cursos futuros
 CURSO_CHANGE_MONTH = 9 # En septiembre cambia el curso académico
+
+try:
+    from local_settings import *
+except ImportError as exp:
+    pass
