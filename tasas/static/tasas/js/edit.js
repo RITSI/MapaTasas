f5f90e817a45f75633c819212d8e74e113627515
Martin <martinarroyo@usal.es>
2016-03-03 20:14:42 +0100

    Eliminación de campos exclusivos antes de POST
    Evita error por datos contradictorios

--- tasas/static/tasas/js/edit.js
+++ tasas/static/tasas/js/edit.js
@@ -1,7 +1,8 @@
 'use strict'
 
 var select_tipo_tasa = document.getElementsByClassName("tipo-tasa-selector");
-
+var form = document.getElementById("tasas-form");
+var submitButton = document.getElementById("submit-tasas");
 var PRECIO_POR_CREDITO = 0;
 var PAGO_UNICO = 1;
 var MISCELANEO = 2;
@@ -11,7 +12,65 @@ classes_tipo_tasa[PRECIO_POR_CREDITO] = "porcredito";
 classes_tipo_tasa[PAGO_UNICO] = "global";
 classes_tipo_tasa[MISCELANEO] = "miscelaneo";
 
+
+
+var remove_contradictory_data = function(option, element){
+    var options = [0,1,2];
+    if(option===undefined || option===""){
+        return remove_contradictory_data([0,1,2], element);
+    }
+
+    if(option == parseInt(option, 10)){
+        options.splice(option, 1);
+      return remove_contradictory_data(options, element);
+    }
+
+    if(option.constructor == Array){
+
+        Array.prototype.forEach.call(option, function(o){
+            var fields;
+            switch (o){
+                case 0:
+                    fields = element.getElementsByClassName("tipo-tasa")[0];
+
+                    break;
+                case 1:
+                    fields = element.getElementsByClassName("tipo-tasa")[1];
+                    break;
+                case 2:
+                    fields = element.getElementsByClassName("tipo-tasa")[2];
+                    break;
+
+            }
+            var input_fields = fields.getElementsByTagName('input');
+            var textarea_fields = fields.getElementsByTagName('textarea');
+
+
+            Array.prototype.forEach.call(input_fields, function(field){
+                field.value = "";
+            });
+            Array.prototype.forEach.call(textarea_fields, function(field){
+                field.value = "";
+            });
+
+        });
+    }
+
+
+};
+
 var initialize = function(){
+    submitButton.addEventListener('click', function(e){
+        //e.preventDefault();
+        var tasas = form.getElementsByClassName('tasas-data');
+        Array.prototype.forEach.call(tasas, function(tasa){
+            var parent = tasa.parentNode.getElementsByClassName('basics')[0];
+            var selector = parent.getElementsByTagName('select')[0];
+            remove_contradictory_data(selector.options[selector.selectedIndex].value, tasa);
+        });
+
+    });
+
     Array.prototype.forEach.call(select_tipo_tasa, function(element){
         var tasas_div = element.parentNode.parentNode.getElementsByClassName('tasas-data')[0];
         var selector = element;
