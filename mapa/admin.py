1c8fe118e381bad3adf2d6d9bbe89030f77c6c61
Martin <martinarroyo@usal.es>
2016-03-06 17:19:11 +0100

    Administración de reportes

--- mapa/admin.py
+++ mapa/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Reporte
 
-# Register your models here.
+class ReporteAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Reporte, ReporteAdmin)
