d3b7e56ca74e5068600fa01b2055d90c7964f99b
Martin <martinarroyo@usal.es>
2016-03-06 12:44:57 +0100

    Archivo .shp con el mapa modificado
    Archivo procedente de naturalearthdata.com, modificado para agrupar las Canarias con la península

---
+++ raw-map/makemap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Clean maps
+
+if [ -f esp.json ]
+then
+mv esp.json esp.json.bak
+fi
+
+if [ -f subunits.json ]
+then
+mv subunits.json subunits.json.bak
+fi
+
+ogr2ogr -f GeoJSON -where "iso_a2 = 'ES'" subunits.json ne_10m_admin_1_states_provinces.shp 
+topojson -o esp.json subunits.json -p name,name_alt --id-property name_alt
