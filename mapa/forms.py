1bbf6d094f0125b720a7223890bf446f6f940e0f
Martin <martinarroyo@usal.es>
2016-03-30 00:29:31 +0200

    Fixes #29

--- mapa/forms.py
+++ mapa/forms.py
@@ -1,11 +1,16 @@
 from django.forms import ModelForm
 from .models import Reporte
+from tasas.models import Universidad
 from django.forms.widgets import Select
 from django.utils.functional import lazy
 from tasas.models import curso_choices
 
 
 class ReporteForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ReporteForm, self).__init__(*args, **kwargs)
+        self.fields['universidad'].choices = [(e.id, "%s - %s" % (e.nombre, e.centro)) for e in Universidad.objects.all()]
+
     class Meta:
         model = Reporte
         exclude = ('id','estado')
