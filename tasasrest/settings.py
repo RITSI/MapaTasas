fa48e30d9acd7404bfb8f126afdebf189ab7310b
Valentin <vk18496@gmail.com>
2017-03-16 13:09:47 +0100

    SECRET_KEY environment variable
    Poder establecer dicha configuración con variables del sistema. Necesario para desplegar con Docker de una forma mas limpia

--- tasasrest/settings.py
+++ tasasrest/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = []
 
 
