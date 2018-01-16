e9e2f3e70caead95f861ba5e825d228973c20c4b
Iñigo Aguas Ardaiz <electroaguas@hotmail.com>
2018-01-16 18:03:08 +0100

    Corregidos los test para funcionar de manera correcta. * Se han desactivado todos los test ligados a la validación de cursos ya que dejaron de realizarse cuando se implementó #32. Será un tema a retomar en algún momento. * Se corrige el curso que ahora es un modelo en vez de un campo. * Se adapta el código de los ficheros afectados a PEP8.

--- tasas/tests.py
+++ tasas/tests.py
@@ -1,12 +1,13 @@
+from datetime import date
+
 from django.test import TestCase
-from django.core.management.base import CommandError
-from mock import patch, Mock, PropertyMock
-from .management.commands import importar
-from .models import Universidad, Tasa, CursoValidator, get_current_curso, curso_choices
-
-import datetime
-
 from django.core.exceptions import ValidationError
+from django.core.management.base import CommandError
+
+from mock import patch
+from .forms import UniversidadForm
+from .management.commands import importar
+from .models import Universidad, Tasa, Curso  # CursoValidator, get_current_curso, curso_choices
 
 
 class TestImportarCommand(TestCase):
@@ -21,7 +22,7 @@ class TestImportarCommand(TestCase):
             self.command.parse_file({})
             self.command.parse_file({'unis': {}})
 
-    #@patch.object(ImageField, 'save')
+    # @patch.object(ImageField, 'save')
     def test_null_keys(self):
 
         invalid_data ={'unis':
@@ -57,77 +58,76 @@ class TestImportarCommand(TestCase):
                 "tasas2": "26.38",
                 "tasas3": "30.44",
                 "tasas4": ""
-            },
-            "tasas_2012": {
-                #"url": "http://www.boe.es/boe/dias/2012/08/14/pdfs/BOE-A-2012-10852.pdf",
-                "url": "",
-                "tasas1": "20.89",
-                "tasas2": "28.90",
-                "tasas3": "54.32",
-                "tasas4": "75.21"
-            },
-            "tasas_2013": {
-                "url": "http://www.boe.es/boe/dias/2013/08/08/pdfs/BOE-A-2013-8800.pdf",
-                "tasas1": "21.24",
-                "tasas2": "29.74",
-                "tasas3": "55.24",
-                "tasas4": "76.48"
-            },
-            "tasas_2014": {
-                "url": "http://www.boe.es/boe/dias/2014/08/07/pdfs/BOE-A-2014-8552.pdf",
-                "tasas1": "22.00",
-                "tasas2": "30.00",
-                "tasas3": "65.00",
-                "tasas4": "90.00"
-            },
-            "tasas_2015": {
-                "url": "http://www.bocm.es/boletin/CM_Orden_BOCM/2015/08/03/BOCM-20150803-15.PDF",
-                "tasas1": "27.90",
-                "tasas2": "51.42",
-                "tasas3": "96.43",
-                "tasas4": "128.57"
-            }}
+                },
+                "tasas_2012": {
+                    "url": "",
+                    "tasas1": "20.89",
+                    "tasas2": "28.90",
+                    "tasas3": "54.32",
+                    "tasas4": "75.21"
+                },
+                "tasas_2013": {
+                    "url": "http://www.boe.es/boe/dias/2013/08/08/pdfs/BOE-A-2013-8800.pdf",
+                    "tasas1": "21.24",
+                    "tasas2": "29.74",
+                    "tasas3": "55.24",
+                    "tasas4": "76.48"
+                },
+                "tasas_2014": {
+                    "url": "http://www.boe.es/boe/dias/2014/08/07/pdfs/BOE-A-2014-8552.pdf",
+                    "tasas1": "22.00",
+                    "tasas2": "30.00",
+                    "tasas3": "65.00",
+                    "tasas4": "90.00"
+                },
+                "tasas_2015": {
+                    "url": "http://www.bocm.es/boletin/CM_Orden_BOCM/2015/08/03/BOCM-20150803-15.PDF",
+                    "tasas1": "27.90",
+                    "tasas2": "51.42",
+                    "tasas3": "96.43",
+                    "tasas4": "128.57"
+                }}
 
         self.command.add_tasas(data,uni)
+
 
 class TestUniversidadModel(TestCase):
 
     def test_valid_strings(self):
         valid_data = (("ual", "ual"),
-        ("uma", "uma"),
-        ("us", "us"),
-        ("upo", "upo"),
-        ("uca", "uca"),
-        ("uco", "uco"),
-        ("uja", "uja"),
-        ("uhu", "uhu"),
-        ("unizar", "unizar"),
-        ("usj", "usj"),
-        ("ulpgc", "ulpgc"),
-        ("ull", "ull"),
-        ("ule", "ule"),
-        ("ugr-granada", "ugr"),
-        ("ugr-ceuta", "ugr"),
-        ("usal-salamanca", "usal"),
-        ("usal-zamora", "usal"),
-        ("uva-valladolid", "uva"),
-        ("uva-segovia", "uva"),
-        ("uo-oviedo", "uo"),
-        ("uo-gijon", "uo"))
-
+                      ("uma", "uma"),
+                      ("us", "us"),
+                      ("upo", "upo"),
+                      ("uca", "uca"),
+                      ("uco", "uco"),
+                      ("uja", "uja"),
+                      ("uhu", "uhu"),
+                      ("unizar", "unizar"),
+                      ("usj", "usj"),
+                      ("ulpgc", "ulpgc"),
+                      ("ull", "ull"),
+                      ("ule", "ule"),
+                      ("uc3m", "uc3m"),
+                      ("ugr-granada", "ugr"),
+                      ("ugr-ceuta", "ugr"),
+                      ("usal-salamanca", "usal"),
+                      ("usal-zamora", "usal"),
+                      ("uva-valladolid", "uva"),
+                      ("uva-segovia", "uva"),
+                      ("uo-oviedo", "uo"),
+                      ("uo-gijon", "uo"))
 
         for data in valid_data:
             self.assertEqual(data[1], Universidad.get_siglas_no_centro(data[0]))
 
     def test_provincia_unicode(self):
-
         uni = Universidad()
         uni.provincia = 'Avila'
 
         self.assertEqual(uni.get_provincia_unicode(), "Ávila")
 
+
 class TestCursoValidator(TestCase):
-    from datetime import date
     class FakeDate(date):
         """A manipulable date replacement"""
         def __new__(cls, *args, **kwargs):
@@ -140,40 +140,44 @@ class TestCursoValidator(TestCase):
     def test_get_current_year(self):
         self.FakeDate.today = classmethod(lambda cls: date(2011, 1, 1))
 
+    # TODO: Volver a activar cuando volvamos a tener una gestión automática de cursos.
+    # @patch('tasas.models.datetime')
+    # def test_get_current_year(self, current_curso_mock):
+    #     current_curso_mock.date.today = Mock(return_value=datetime.datetime.strptime('Sep 1 2011', '%b %d %Y'))
+    #     self.assertEqual(2011, get_current_curso())
+    #
+    #     current_curso_mock.date.today = Mock(return_value=datetime.datetime.strptime('Aug 31 2011', '%b %d %Y'))
+    #
+    #     self.assertEqual(2010, get_current_curso())
+    #
+    # @patch('tasas.models.datetime')
+    # #@patch('tasas.models.settings.MIN_YEAR')
+    # def test_curso_validator(self, current_curso_mock):
+    #     validator = CursoValidator()
+    #     current_curso_mock.date.today = Mock(return_value=datetime.datetime.strptime('Sep 1 2011', '%b %d %Y'))
+    #
+    #     #type(settings_mock).MIN_YEAR = PropertyMock(return_value=2011)
+    #     #settings_mock.configure_mock(MIN_YEAR=2011)
+    #     #settings_mock.YEARS_IN_ADVANCE = 1
+    #     #settings_mock.CURSO_CHANGE_MONTH = 10
+    #
+    #     self.assertRaises(ValidationError, validator.__call__,(2010))
+    #
+    #     current_curso_mock.date.today = Mock(return_value=datetime.datetime.strptime('Aug 1 2011', '%b %d %Y'))
+    #     self.assertEqual(None, validator.__call__(2011))
+    #
+    #     self.assertRaises(ValidationError, validator.__call__, (2013))
+    #
+    # @patch('tasas.models.datetime')
+    # def test_curso_choices(self, current_curso_mock):
+    #     current_curso_mock.date.today = Mock(return_value=datetime.datetime.strptime('Sep 1 2011', '%b %d %Y'))
+    #     self.assertEqual(((2011, '2011/2012'), (2012, '2012/2013')), curso_choices())
 
-    @patch('tasas.models.datetime')
-    def test_get_current_year(self, current_curso_mock):
-        current_curso_mock.date.today = Mock(return_value=datetime.datetime.strptime('Sep 1 2011', '%b %d %Y'))
-        self.assertEqual(2011, get_current_curso())
-
-        current_curso_mock.date.today = Mock(return_value=datetime.datetime.strptime('Aug 31 2011', '%b %d %Y'))
-
-        self.assertEqual(2010, get_current_curso())
-
-    @patch('tasas.models.datetime')
-    #@patch('tasas.models.settings.MIN_YEAR')
-    def test_curso_validator(self, current_curso_mock):
-        validator = CursoValidator()
-        current_curso_mock.date.today = Mock(return_value=datetime.datetime.strptime('Sep 1 2011', '%b %d %Y'))
-
-        #type(settings_mock).MIN_YEAR = PropertyMock(return_value=2011)
-        #settings_mock.configure_mock(MIN_YEAR=2011)
-        #settings_mock.YEARS_IN_ADVANCE = 1
-        #settings_mock.CURSO_CHANGE_MONTH = 10
-
-        self.assertRaises(ValidationError, validator.__call__,(2010))
-
-        current_curso_mock.date.today = Mock(return_value=datetime.datetime.strptime('Aug 1 2011', '%b %d %Y'))
-        self.assertEqual(None, validator.__call__(2011))
-
-        self.assertRaises(ValidationError, validator.__call__, (2013))
-
-    @patch('tasas.models.datetime')
-    def test_curso_choices(self, current_curso_mock):
-        current_curso_mock.date.today = Mock(return_value=datetime.datetime.strptime('Sep 1 2011', '%b %d %Y'))
-        self.assertEqual(((2011, '2011/2012'), (2012, '2012/2013')), curso_choices())
 
 class TestTasaModel(TestCase):
+    curso = Curso()
+    curso.anno = 2011
+
     def setUp(self):
         pass
 
@@ -189,7 +193,7 @@ class TestTasaModel(TestCase):
         self.assertRaises(ValidationError, tasa.clean_fields)
         tasa.universidad = Universidad()
         self.assertRaises(ValidationError, tasa.clean_fields)
-        tasa.curso = 2011
+        tasa.curso = self.curso
 
         self.assertRaises(ValidationError, tasa.clean)
         tasa.tasas1 = 10
@@ -213,7 +217,7 @@ class TestTasaModel(TestCase):
         self.assertRaises(ValidationError, tasa.clean_fields)
         tasa.universidad = Universidad()
         self.assertRaises(ValidationError, tasa.clean_fields)
-        tasa.curso = 2011
+        tasa.curso = self.curso
 
         self.assertRaises(ValidationError, tasa.clean)
         tasa.tasas1 = 10
@@ -239,7 +243,7 @@ class TestTasaModel(TestCase):
         self.assertRaises(ValidationError, tasa.clean_fields)
         tasa.universidad = Universidad()
         self.assertRaises(ValidationError, tasa.clean_fields)
-        tasa.curso = 2011
+        tasa.curso = self.curso
 
         self.assertRaises(ValidationError, tasa.clean)
         tasa.tasa_global = 10
@@ -266,7 +270,7 @@ class TestTasaModel(TestCase):
         self.assertRaises(ValidationError, tasa.clean_fields)
         tasa.universidad = Universidad()
         self.assertRaises(ValidationError, tasa.clean_fields)
-        tasa.curso = 2011
+        tasa.curso = self.curso
 
         self.assertRaises(ValidationError, tasa.clean)
         tasa.descripcion = "Descripción"
@@ -296,31 +300,17 @@ class TestIncrementTemplate(TestCase):
 
     def test_increment_valid(self):
         context = Context({'12aa': '12aa'})
+        self.assertEqual('2', Template('{% load increment %}' '{% increment 1%}').render(context))
+        self.assertRaises(TemplateSyntaxError, Template, '{% load increment %}' '{% increment 1 2 3%}')
+        self.assertRaises(TemplateSyntaxError, Template, '{% load increment %}' '{% increment '' %}')
+        self.assertRaises(TemplateSyntaxError, Template('{% load increment %}' '{% increment 12aa %}').render, context)
 
-        self.assertEqual('2', Template(
-            '{% load increment %}'
-            '{% increment 1%}'
-        ).render(context))
-
-        self.assertRaises(TemplateSyntaxError, Template, ('{% load increment %}'
-            '{% increment 1 2 3%}'))
-
-        self.assertRaises(TemplateSyntaxError, Template, ('{% load increment %}''{% increment '' %}'))
-
-        self.assertRaises(TemplateSyntaxError, Template('{% load increment %}''{% increment 12aa %}').render, context)
-
-
-from .forms import UniversidadForm
 
 class TestUniversidadForm(TestCase):
     # The form is almost entirely managed by Django, so this test makes little sense
     def test_valid_data(self):
-        form = UniversidadForm({
-
-        })
+        form = UniversidadForm({})
 
         self.assertFalse(form.is_valid())
-        #self.assertEqual(form.errors, {
-
-        #})
+        # self.assertEqual(form.errors, {})
 
