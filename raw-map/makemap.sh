86671f1ca3bc470df31c88388f55f4fc6313bb6a
rexuswolf <francisjmp99@gmail.com>
2019-04-12 20:26:41 +0200

    Correcciones script makemap y coding de settings.py

--- raw-map/makemap.sh
+++ raw-map/makemap.sh
@@ -12,4 +12,4 @@ mv subunits.json subunits.json.bak
 fi
 
 ogr2ogr -f GeoJSON -where "iso_a2 = 'ES'" subunits.json ne_10m_admin_1_states_provinces.shp 
-topojson -o esp.json subunits.json -p name,name_alt --id-property name_alt
+geo2topo -o esp.json subunits.json
